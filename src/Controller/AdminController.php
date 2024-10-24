71e2f54d9d4d5ab51bc53c19f293cb33fb804574
EduVarga <eruk11000@gmail.com>
2024-10-24 12:55:36 +0200

    Primer commit

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Form\CategoryFormType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Persistence\ManagerRegistry;
+
+class AdminController extends AbstractController
+{
+    #[Route('/admin/images', name: 'app_images')]
+    public function images(): Response
+    {
+        return $this->render('admin/images.html.twig', []);
+    }
+
+    #[Route('/admin', name: 'admin')]
+    public function adminDashboard(): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        // or add an optional message - seen by developers
+        $this->denyAccessUnlessGranted('ROLE_ADMIN', null, 'User tried to access a page without having ROLE_ADMIN');
+
+        return new Response("Sí que puedes entrar");
+    }
+
+    #[Route('/admin/categories', name: 'app_categories')]
+    public function categories(ManagerRegistry $doctrine, Request $request): Response
+    {
+        $repositorio = $doctrine->getRepository(Category::class);
+
+        $categories = $repositorio->findAll();
+
+        $category = new Category();
+        $form = $this->createForm(CategoryFormType::class, $category);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $category = $form->getData();
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($category);
+            $entityManager->flush();
+        }
+        return $this->render('admin/categories.html.twig', array(
+            'form' => $form->createView(),
+            'categories' => $categories
+        ));
+
+    }
+
+
+
+
+}
